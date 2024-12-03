9c0104de78df2a87b464f03c5966e952a1365f3d
Mhar Andal <mhar@codeverse.com>
2024-12-03 11:34:12 -0600

    feat: support pdfs and link image previews

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        canvas: './empty-module.ts',
+      },
+    },
+  },
   /* config options here */
-};
+  images: {
+    domains: [
+      'api.microlink.io', // Microlink Image Preview
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
